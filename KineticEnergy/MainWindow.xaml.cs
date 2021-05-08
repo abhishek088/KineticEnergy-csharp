b0e1ad673a5544b871baebdb1b79bb69ffedd17d
abhsihek088 <sawantabhishek08@gmail.com>
2021-05-08 18:40:03 -0400

    Add project files.

---
+++ KineticEnergy/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿//Author: Group 1: Abhishek Sawant, Konrad Gaerdes, Rupal Pandya
+
+using System.Windows;
+
+namespace KineticEnergy
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        VM vm = new VM();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = vm;
+        }
+
+        private void Calculate_Click(object sender, RoutedEventArgs e)
+        {
+            vm.CalcKineticEnergy();
+        }
+
+        private void Clear_Click(object sender, RoutedEventArgs e)
+        {
+            vm.ClearEverything();
+        }
+    }
+}
---
+++ KineticEnergy/VM.cs
@@ -0,0 +1,78 @@
+﻿//Author: Group 1: Abhishek Sawant, Konrad Gaerdes, Rupal Pandya
+
+using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace KineticEnergy
+{
+    class VM : INotifyPropertyChanged
+    {
+        const decimal FORMULA_CONSTANT = 0.5m;
+
+        public void CalcKineticEnergy() //Method to calculate kinetic energy
+        {
+            try
+            {
+                ErrorMessage = "";
+
+                decimal MassKgInDecimal = decimal.Parse(MassKg);
+                decimal VelocityMsInDecimal = decimal.Parse(VelocityMs);
+
+                if (MassKgInDecimal < 0)
+                    ErrorMessage = "Mass cannot be a negative value";
+                else
+                    KineticEnergy = FORMULA_CONSTANT * MassKgInDecimal * VelocityMsInDecimal * VelocityMsInDecimal;
+            }
+            catch
+            {
+                ErrorMessage = "Please enter a valid numeric input";
+            }
+        }
+
+        public void ClearEverything() //Method to clear everything
+        {
+            MassKg = "0";
+            VelocityMs = "0";
+            KineticEnergy = 0;
+            ErrorMessage = "";
+        }
+
+        #region PropertyChanged
+        private string massKg = "0";
+        public string MassKg
+        {
+            get { return massKg; }
+            set { massKg = value; notifyChange(); }
+        }
+
+        private string velocityMs = "0";
+        public string VelocityMs
+        {
+            get { return velocityMs; }
+            set { velocityMs = value; notifyChange(); }
+        }
+
+        private decimal kineticEnergy = 0;
+        public decimal KineticEnergy
+        {
+            get { return kineticEnergy; }
+            set { kineticEnergy = value; notifyChange(); }
+        }
+
+        private string errorMessage = "";
+        public string ErrorMessage
+        {
+            get { return errorMessage; }
+            set { errorMessage = value; notifyChange(); }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void notifyChange([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+    }
+}
